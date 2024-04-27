baf2aca49a2b3a2d280ae35988be9cf107136378
Dhruv <61105515+dbiscoding@users.noreply.github.com>
2024-04-27 00:02:15 -0700

    Refactored Follows entity and add User entity

--- src/main/java/com/cookit/backend/entity/Recipe.java
+++
@@ -1,5 +0,0 @@
-package com.cookit.backend.entity;
-
-public class Recipe {
-
-}
--- src/main/java/com/cookit/backend/entity/User.java
+++ src/main/java/com/cookit/backend/entity/User.java
@@ -1,5 +1,36 @@
 package com.cookit.backend.entity;
 
-public class User {
+import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "join_date", nullable = false)
+    private LocalDate joinDate;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "profile_picture")
+    private String profilePicture;
 }
+
